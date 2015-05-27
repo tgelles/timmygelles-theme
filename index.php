5c397bae19c43d946f4c0cfb4e27190f35bcea4b
tgelles <tgelles@pha.jhu.edu>
2015-05-27 18:02:47 -0400

    blog style updates

--- index.php
+++ index.php
@@ -11,4 +11,15 @@
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
 
-<?php the_posts_navigation(); ?>
+<div class="row">
+	<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+		<?php if ($wp_query->max_num_pages > 1) : ?>
+			  <nav class="post-nav">
+			    <ul class="pager">
+			      <li class="next"><?php previous_posts_link(__('<span class="glyphicon glyphicon glyphicon-menu-left" aria-hidden="true"></span> Newer posts', 'roots')); ?></li>
+			      <li class="previous"><?php next_posts_link(__('Older posts <span class="glyphicon glyphicon glyphicon-menu-right" aria-hidden="true"></span>', 'roots')); ?></li>
+			    </ul>
+			  </nav>
+		<?php endif; ?>
+	</div>
+</div>
--- templates/content.php
+++ templates/content.php
@@ -3,7 +3,7 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
-  <div class="entry-summary">
+  <h3 class="entry-summary">
     <?php the_excerpt(); ?>
-  </div>
+  </h3>
 </article>
